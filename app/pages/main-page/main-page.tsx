70af31b118b1e0480905e18c746ee0f6c8ba7dca
ethereal element <vitalik.safonov@list.ru>
2023-12-07 18:06:06 +0300

    create: layout main page

--- app/pages/main-page/main-page.tsx
+++ app/pages/main-page/main-page.tsx
@@ -1,23 +1,22 @@
 "use client";
 import { FC } from 'react';
-import {Layout, Space} from "antd";
-import {Sider,Header,Content,Footer, siderStyle, headerStyle, contentStyle,footerStyle} from "@/app/pages/main-page/main-page.styles";
 import {Sidebar} from "@/app/components/sidebar/sidebar";
 import {AppHeader} from "@/app/components/header/header";
+import styles from "./main-page.module.scss";
 
 export const MainPage: FC = () => {
-    return <Space direction={"vertical"} style={{width: "100%",minHeight: '100vh' }} >
-        <Layout>
-            <Sider style={siderStyle}>
-                <Sidebar></Sidebar>
-            </Sider>
-            <Layout>
-                <Header style={headerStyle}>
-                    <AppHeader></AppHeader>
-                </Header>
-                <Content style={contentStyle}>Content</Content>
-                <Footer style={footerStyle}>Footer</Footer>
-            </Layout>
-        </Layout>
-    </Space>;
+    return <div className={styles.gridContainer}>
+        <div className={styles.sidebar}>
+            <Sidebar></Sidebar>
+        </div>
+        <div className={styles.header}>
+            <AppHeader></AppHeader>
+        </div>
+        <div className={styles.content}>
+           Content
+        </div>
+        <div className={styles.footer}>
+            Footer
+        </div>
+    </div>;
 };
