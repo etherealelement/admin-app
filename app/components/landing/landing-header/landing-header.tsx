7088d236f166a67fd090d854bb6f9abcefc9277e
safonov-work <safonov.itrum@gmail.com>
2023-12-12 14:29:26 +0300

    add: routers

--- app/components/landing/landing-header/landing-header.tsx
+++ app/components/landing/landing-header/landing-header.tsx
@@ -1,32 +1,46 @@
 import { FC } from 'react';
-import styles from "./landing-header.module.scss";
-import {IconLogo} from "@/app/components/ui/icons/icon-logo";
-import Link from "next/link";
-import {Button} from "@/app/components/ui/button/button";
-import "../../../globals.css";
+import styles from './landing-header.module.scss';
+import { IconLogo } from '@/app/components/ui/icons/icon-logo';
+import Link from 'next/link';
+import { Button } from '@/app/components/ui/button/button';
+import '../../../globals.css';
 export const LandingHeader: FC = () => {
-    return <header className={styles.header}>
-        <div className={"container"}>
-            <div className={styles.headerInner}>
-                <div className={styles.headerInnerLogo}>
-                    <Link href={"/"}>
-                        <IconLogo></IconLogo>
-                    </Link>
-                </div>
-                <nav className={styles.headerInnerNav}>
-                    <ul className={styles.headerInnerNavList}>
-                        <li className={styles.headerInnerNavListItem}>
-                            <Link href={"/"} className={styles.headerInnerNavListItemLinkActive}>Schedule demo</Link>
-                        </li>
-                        <li className={styles.headerInnerNavListItem}>
-                            <Link href={"/"} className={styles.headerInnerNavListItemLink}>Login</Link>
-                            </li>
-                        <li className={styles.headerInnerNavListItem}>
-                            <Button type={"signup"}>Sign up for free</Button>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+  return (
+    <header className={styles.header}>
+      <div className={'container'}>
+        <div className={styles.headerInner}>
+          <div className={styles.headerInnerLogo}>
+            <Link href={'/'}>
+              <IconLogo></IconLogo>
+            </Link>
+          </div>
+          <nav className={styles.headerInnerNav}>
+            <ul className={styles.headerInnerNavList}>
+              <li className={styles.headerInnerNavListItem}>
+                <Link
+                  href={'/'}
+                  className={styles.headerInnerNavListItemLinkActive}
+                >
+                  Schedule demo
+                </Link>
+              </li>
+              <li className={styles.headerInnerNavListItem}>
+                <Link
+                  href={'/pages/auth-page'}
+                  className={styles.headerInnerNavListItemLink}
+                >
+                  Login
+                </Link>
+              </li>
+              <li className={styles.headerInnerNavListItem}>
+                <Link href={'/pages/reg-page'}>
+                  <Button type={'signup'}>Sign up for free</Button>
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-    </header>;
+      </div>
+    </header>
+  );
 };
---
+++ app/pages/reg-page/reg-page.tsx
@@ -0,0 +1,5 @@
+import { FC } from 'react';
+
+export const RegPage: FC = (): JSX.Element => {
+  return <></>;
+};
